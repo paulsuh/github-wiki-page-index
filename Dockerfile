baa3fb73bf351e887fd005c634211c1ffd37faca
Paul Suh <paul@mspex.net>
2022-04-25 01:02:41 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10-alpine
+
+RUN apk add git
+
+RUN mkdir /checkoutdir
+
+COPY entrypoint.sh /entrypoint.sh
+COPY generate_wiki_file_index.py /generate_wiki_file_index.py
+
+RUN /bin/chmod a+x /entrypoint.sh
+RUN /bin/chmod a+x /generate_wiki_file_index.py
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Inputs from environment variables
+#
+# REPO_NAME: https URL used to clone the wiki
+#
+# AUTH_TOKEN: authentication token to allow content to be pushed
+#
+# USERNAME: Username for use with the AUTH_TOKEN
+#
+# USER_EMAIL: Email address of the user. Utilized as part of the commit action
+
+
+# clone the wiki
+cd /checkoutdir
+
+git clone "https://${AUTH_TOKEN}@github.com/${REPO_NAME}.wiki.git"
+
+ls
+
+cd $(basename "${REPO_NAME}.wiki")
+
+git config user.name "${USERNAME}"
+git config user.email "${USER_EMAIL}"
+
+# run the script to produce a new file list in Home.md
+python /generate_wiki_file_index.py -i "/checkoutdir/$(basename "${REPO_NAME}.wiki")"
+
+# commit and push the change back to the repo
+git add Home.md
+git commit -m "Auto-generate wiki file list."
+git push
+
