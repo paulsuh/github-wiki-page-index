822985516d9402cb33a482a47b1bf23e56767a9f
Paul Suh <paul@mspex.net>
2022-04-27 15:54:20 -0400

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='githubwikipageindex',
+    version='0.2',
+    packages=['githubwikipageindex'],
+    url='https://github.com/paulsuh/githubwikipageindex',
+    license='BSD 3-clause',
+    author='Paul Suh',
+    author_email='paul@mspex.net',
+    description='Build a page index for a GitHub Wiki'
+)
